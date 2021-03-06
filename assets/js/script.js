738d99720d6577bbb5df772411c341a5544fbf30
sshahram <shirin.shahram23@gmail.com>
2021-03-05 23:38:08 -0800

    add current date to page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// function to display current date
+var displayCurrentDate = function () {
+var currentDate = moment().format("LL");
+var currentDay = $("#currentDay").text(currentDate);
+return currentDay;
+};
+
+displayCurrentDate();
