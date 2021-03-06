efe9642b484f6fb6070228d2a31da1c0df597619
sshahram <shirin.shahram23@gmail.com>
2021-03-06 01:21:19 -0800

    add localStorage feature to all hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,85 @@ return currentDay;
 // save task in local storage
 $("#btn1").on("click", function(event) {
     event.preventDefault();
-    var inputVal = $("#input1").val().trim();
-    localStorage.setItem("task1", JSON.stringify(inputVal))
+    var inputVal1 = $("#input1").val().trim();
+    localStorage.setItem("task1", JSON.stringify(inputVal1))
 });
 
+$("#btn2").on("click", function(event) {
+    event.preventDefault();
+    var inputVal2 = $("#input2").val().trim();
+    localStorage.setItem("task2", JSON.stringify(inputVal2))
+});
+
+$("#btn3").on("click", function(event) {
+    event.preventDefault();
+    var inputVal3 = $("#input3").val().trim();
+    localStorage.setItem("task3", JSON.stringify(inputVal3))
+});
+
+$("#btn4").on("click", function(event) {
+    event.preventDefault();
+    var inputVal4 = $("#input4").val().trim();
+    localStorage.setItem("task4", JSON.stringify(inputVal4))
+});
+
+$("#btn5").on("click", function(event) {
+    event.preventDefault();
+    var inputVal5 = $("#input5").val().trim();
+    localStorage.setItem("task5", JSON.stringify(inputVal5))
+});
+
+$("#btn6").on("click", function(event) {
+    event.preventDefault();
+    var inputVal6 = $("#input6").val().trim();
+    localStorage.setItem("task6", JSON.stringify(inputVal6))
+});
+
+$("#btn7").on("click", function(event) {
+    event.preventDefault();
+    var inputVal7 = $("#input7").val().trim();
+    localStorage.setItem("task7", JSON.stringify(inputVal7))
+});
+
+$("#btn8").on("click", function(event) {
+    event.preventDefault();
+    var inputVal8 = $("#input8").val().trim();
+    localStorage.setItem("task8", JSON.stringify(inputVal8))
+});
+
+$("#btn9").on("click", function(event) {
+    event.preventDefault();
+    var inputVal9 = $("#input9").val().trim();
+    localStorage.setItem("task9", JSON.stringify(inputVal9))
+});
+
+
 // retrieve task from localStorage
-// var savedTask1 = localStorage.getItem("task1");
-// inputVal.val(savedTask1);
 var savedTask1 = JSON.parse(localStorage.getItem("task1"));
 $("#input1").val(savedTask1);
+
+var savedTask2 = JSON.parse(localStorage.getItem("task2"));
+$("#input2").val(savedTask2);
+
+var savedTask3 = JSON.parse(localStorage.getItem("task3"));
+$("#input3").val(savedTask3);
+
+var savedTask4 = JSON.parse(localStorage.getItem("task4"));
+$("#input4").val(savedTask4);
+
+var savedTask5 = JSON.parse(localStorage.getItem("task5"));
+$("#input5").val(savedTask5);
+
+var savedTask6 = JSON.parse(localStorage.getItem("task6"));
+$("#input6").val(savedTask6);
+
+var savedTask7 = JSON.parse(localStorage.getItem("task7"));
+$("#input7").val(savedTask7);
+
+var savedTask8 = JSON.parse(localStorage.getItem("task8"));
+$("#input8").val(savedTask8);
+
+var savedTask9 = JSON.parse(localStorage.getItem("task9"));
+$("#input9").val(savedTask9);
 
 displayCurrentDate();
